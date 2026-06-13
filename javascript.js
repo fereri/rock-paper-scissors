1cea31ac07af127f4554f639c182ef3114478016
David <250323761+fereri@users.noreply.github.com>
2026-06-13 20:29:05 +0200

    Player function is completed.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
 let rand = Math.random()
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice(rand) {
     if (rand >= 0.7) {
-        return "Rock"
+        return "rock"
     } else if (rand >= 0.4 && rand < 0.7) {
-        return "Scissor"
+        return "scissor"
     } else if (rand < 0.4) {
-        return "Paper"
+        return "paper"
     }
 }
 
-console.log(rand)
-console.log(getComputerChoice(rand))
+function getHumanChoice() {
+    let choice = prompt("What do you pick")
+
+    switch (choice) {
+        case "rock" :
+            return "rock"
+            break
+
+        case "paper" :
+            return "paper"
+            break
+
+        case "scissor" :
+            return "scissor"
+            break
+    }
+}
+
+console.log(getHumanChoice())
